1f65aeb8a9fca7f317b8ce174e61eb2c55e9c272
HanseGucker <joniweth@gmx.de>
2018-11-18 15:56:52 +0100

    Initial build

---
+++ precache-manifest.cb2fcefb9a4ca67d1fcd812bdac7d6f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90528eb7d04ebeacaf44",
+    "url": "/static/js/main.90528eb7.chunk.js"
+  },
+  {
+    "revision": "d40c5f11feaf11ca5b46",
+    "url": "/static/js/1.d40c5f11.chunk.js"
+  },
+  {
+    "revision": "90528eb7d04ebeacaf44",
+    "url": "/static/css/main.09180a5c.chunk.css"
+  },
+  {
+    "revision": "d40c5f11feaf11ca5b46",
+    "url": "/static/css/1.576cd039.chunk.css"
+  },
+  {
+    "revision": "f8ba5ff59687059c3235f7627d3e9d07",
+    "url": "/index.html"
+  }
+];
